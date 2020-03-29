40dae38ce7b2577b64ef89ecf29d7a50d155fc6b
Liza <o4kastar.panda@gmail.com>
2020-03-29 22:29:53 +0500

    заполнение базы данных DbTimetable. добавление элементов в comboBox.

--- controllers/controllers_main.py
+++ controllers/controllers_main.py
@@ -1,6 +1,9 @@
+from db_timetable.database import *
 from views.gui_timetable import *
 from views.gui_dialog_update import *
 from views.gui_dialog_add import *
+from pony.orm import *
+
 import sys
 
 
@@ -20,6 +23,13 @@ class MainForm(QtWidgets.QMainWindow):
         self.ui.tableWidget.setHorizontalHeaderLabels(
             ('День недели', 'Номер урока', 'Дисциплина', 'Класс', 'Кабинет', 'Учитель')
         )
+        with db_session:
+            self.ui.comboBox_3.addItems(select(d.name for d in Discipline))
+            self.ui.comboBox.addItems(select(dw.name for dw in DayWeek))
+            self.ui.comboBox_4.addItems(select(c.full_class for c in Class))
+            self.ui.comboBox_5.addItems(select(c.number+" корпус "+c.housing.name for c in Cabinet))
+            self.ui.comboBox_2.addItems(select(str(l.number) for l in LessonNumber))
+            self.ui.comboBox_6.addItems(select(t.full_name for t in Teacher))
 
     def open_dialog_add(self):
         dialog = QtWidgets.QDialog()
